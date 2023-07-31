08299b771e3d7c80cf3a17ee718c82e67d3d9d3e
JiangTaoHou <925147954@qq.com>
2023-07-31 14:53:05 +0800

    增加注释

--- src/main/java/javaprogramming/commonmistakes/concurrenttool/concurrenthashmapmisuse/ConcurrentHashMapMisuseController.java
+++ src/main/java/javaprogramming/commonmistakes/concurrenttool/concurrenthashmapmisuse/ConcurrentHashMapMisuseController.java
@@ -23,6 +23,7 @@ public class ConcurrentHashMapMisuseController {
     private static int ITEM_COUNT = 1000;
 
     private ConcurrentHashMap<String, Long> getData(int count) {
+
         return LongStream.rangeClosed(1, count)
                 .boxed()
                 .collect(Collectors.toConcurrentMap(i -> UUID.randomUUID().toString(), Function.identity(),
@@ -31,11 +32,18 @@ public class ConcurrentHashMapMisuseController {
 
     @GetMapping("wrong")
     public String wrong() throws InterruptedException {
+        //生成你给定定长度定HashMap,这里是900个，
         ConcurrentHashMap<String, Long> concurrentHashMap = getData(ITEM_COUNT - 100);
         log.info("init size:{}", concurrentHashMap.size());
-
+        //线程的个数
         ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
         forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            //concurrentHashMap可以保证多个线程添加的时候不会发生覆盖的情况，但是各个操作都是添加
+            //ConcurrentHashMap 这个篮子本身，可以确保多个工人在装东西进去时，不会相互影响干扰，
+            // 但无法确保工人 A 看到还需要装 100 个桔子但是还未装的时候，
+            // 工人 B 就看不到篮子中的桔子数量。更值得注意的是，
+            // 你往这个篮子装 100 个桔子的操作不是原子性的，
+            // 在别人看来可能会有一个瞬间篮子里有 964 个桔子，还需要补 36 个桔子。
             int gap = ITEM_COUNT - concurrentHashMap.size();
             log.info("gap size:{}", gap);
             concurrentHashMap.putAll(getData(gap));
@@ -54,7 +62,10 @@ public class ConcurrentHashMapMisuseController {
 
         ForkJoinPool forkJoinPool = new ForkJoinPool(THREAD_COUNT);
         forkJoinPool.execute(() -> IntStream.rangeClosed(1, 10).parallel().forEach(i -> {
+            //先锁住这段逻辑，确保只有一个能进来
             synchronized (concurrentHashMap) {
+                //其他的进来之后，第一个已经填充到1000了，那么这里获取到需要填充的个数就是0
+                //所以就是线程安全的。
                 int gap = ITEM_COUNT - concurrentHashMap.size();
                 log.info("gap size:{}", gap);
                 concurrentHashMap.putAll(getData(gap));
